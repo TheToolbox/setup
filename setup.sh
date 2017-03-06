7b09e1bcac7b0a6abe883a44b4f798a96e3c3c68
Toolbox <jason@oettinger.email>
2017-03-05 23:13:27 -0500

    adding requirement

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ cp setup/.bash_aliases . &&
 rm -rf $HOME/setup 
 
 #set up lastpass cli
-sudo apt-get -y install openssl libcurl4-openssl-dev libxml2 libssl-dev libxml2-dev pinentry-curses xclip cmake make gcc &&
+sudo apt-get -y install openssl libcurl4-openssl-dev libxml2 libssl-dev libxml2-dev pinentry-curses xclip cmake make gcc pkg-config &&
 git clone https://github.com/lastpass/lastpass-cli.git &&
 cd lastpass-cli &&
 cmake . && make &&
