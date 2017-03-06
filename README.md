22da577a06659d19aaffcba82968332c463911f9
Toolbox <jason@oettinger.email>
2017-03-05 21:37:41 -0500

    Fixing apt install, adding instructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Setup
+
+To use:
+
+`curl https://raw.githubusercontent.com/TheToolbox/setup/master/setup.sh | sh`
+
 Sets up my account on new machines, allows my keys, checks if I have sudo, installs the applications I like, and adds candy to my bash
+
+#
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Installing git"
-sudo apt-get install git 
-echo "Moving to homedir"
-cd $HOME
-git clone https://github.com/TheToolbox/setup.git
+echo "Installing git" &&
+sudo apt-get install git -y &&
+echo "Moving to homedir" &&
+cd $HOME &&
+git clone https://github.com/TheToolbox/setup.git &&
 
-cp setup/.bashrc .
-cp setup/.bash_aliases .
-rm -rf $HOME/setup
+cp setup/.bashrc . &&
+cp setup/.bash_aliases . &&
+rm -rf $HOME/setup &&
