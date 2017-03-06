dbe077ce372b3e83186925bb81a68725af81da11
Toolbox <jason@oettinger.email>
2017-03-05 22:47:18 -0500

    removing unnecessary bits

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Installing git" && \
-sudo apt-get update && \
-sudo apt-get -y install git && \
-echo "Moving to homedir" && \
-cd $HOME && \
-git clone https://github.com/TheToolbox/setup.git && \
-cp setup/.bashrc . && \
-cp setup/.bash_aliases . && \
+echo "Installing git" && 
+sudo apt-get update && 
+sudo apt-get -y install git && 
+
+echo "Moving to homedir" && 
+cd $HOME && 
+git clone https://github.com/TheToolbox/setup.git && 
+
+cp setup/.bashrc . && 
+cp setup/.bash_aliases . && 
 rm -rf $HOME/setup 
