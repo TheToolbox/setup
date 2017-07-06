400ef5531bba8958573b4832be7b0961a40c7fd3
Jason Oettinger <jason@oettinger.email>
2017-07-06 16:37:30 -0400

    Adding g++

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ cp -f setup/.gitconfig .
 rm -rf $HOME/setup 
 
 #set up lastpass cli
-sudo apt-get -y install openssl libcurl4-openssl-dev libxml2 libssl-dev libxml2-dev pinentry-curses xclip cmake make gcc pkg-config &&
+sudo apt-get -y install openssl libcurl4-openssl-dev libxml2 libssl-dev libxml2-dev pinentry-curses xclip cmake make gcc g++ pkg-config &&
 git clone https://github.com/lastpass/lastpass-cli.git &&
 cd lastpass-cli &&
 cmake . && make &&
